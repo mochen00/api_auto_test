449bcc32fda4a86ef7813e1bfea5cfe91ce5d629
zhaofan03 <zhaofan03@kuaishou.com>
2020-05-18 17:05:06 +0800

    fix path

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -3,6 +3,7 @@ import pytest
 import allure
 import yaml
 import os
+from utils import get_file_path
 '''
 @author: lingshu
 @file: conftest.py
@@ -15,13 +16,14 @@ def env_config(request):
     读取yml配置文件
     """
     project_name = 'api_auto_test'
-    rootPath = get_root_path(project_name)
+    rootPath = get_file_path.get_root_path()
+    print(rootPath)
     config_path = os.path.abspath(rootPath + 'config/env_config.yml')  # 获取tran.csv文件的路径
     with open(config_path) as f:
         env_config = yaml.load(f) #读取配置文件
     return env_config
 
-def get_root_path(project_name):
-    curPath = os.path.abspath(os.path.dirname(__file__))
-    rootPath = curPath[:curPath.find(project_name+"/") + len(project_name+"/")]  # 获取myProject，也就是项目的根路径
-    return rootPath
+# def get_root_path(project_name):
+#     curPath = os.path.abspath(os.path.dirname(__file__))
+#     rootPath = curPath[:curPath.find(project_name+"/") + len(project_name+"/")]  # 获取myProject，也就是项目的根路径
+#     return rootPath
