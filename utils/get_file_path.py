24c58549253c56fc831f11a95fc900d7d7e8cc32
zhaofan03 <zhaofan03@kuaishou.com>
2020-05-18 16:36:17 +0800

    log

--- utils/get_file_path.py
+++ utils/get_file_path.py
@@ -15,7 +15,9 @@ def get_root_path():
     :return:
     '''
     curPath = os.path.abspath(os.path.dirname(__file__))
+    print(curPath)
     rootPath = curPath[:curPath.find(project_name+"/") + len(project_name+"/")]  # 获取myProject，也就是项目的根路径
+    print(rootPath)
     # print("rootPath= %s",rootPath)
     return rootPath
 
